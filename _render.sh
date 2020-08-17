a09d854366151bfe3db71e53719888d058296325
Daniel McCloy <dan@mccloy.info>
2020-08-16 18:32:00 -0700

    fix bibliography rendering

--- _render.sh
+++ _render.sh
@@ -1,2 +1,3 @@
 Rscript -e "rmarkdown::render('_faq.Rmd', output_format=rmarkdown::md_document(variant='gfm', preserve_yaml=TRUE), output_file='faq.md')"
+#Rscript -e "rmarkdown::render('_faq.Rmd', output_file='faq.html')"
 # git add faq.md images/faq/*.png
