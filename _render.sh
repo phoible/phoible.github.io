c23ea2776cf5c77be006d68eaac65d87fd7b00ff
Daniel McCloy <dan@mccloy.info>
2020-11-17 13:14:20 -0600

    Finish FAQ (#26)
    * fill in TODOs on FAQ
    
    * reflow long lines
    
    * fix typo
    
    * add rendered FAQ
    
    * add header link to FAQ
    
    * remove redunancy
    
    * fix chronology
    
    * add version info
    
    * add colspecs
    
    * minor cleanups
    
    * use built-in TOC
    
    * fix param name
    
    * separate section for loading data
    
    * code clean-ups
    
    * fix unnamed chunk
    
    * re-render
    
    * don't link to rendered CSV (file too big to render)
    
    * rerender md file
    
    * better section organization; crossref; re-render md file
    
    * fix bibtex
    
    * fix: make TOC appear in markdown doc

--- _render.sh
+++
@@ -1,3 +0,0 @@
-Rscript -e "rmarkdown::render('_faq.Rmd', output_format=rmarkdown::md_document(variant='gfm', preserve_yaml=TRUE), output_file='faq.md')"
-#Rscript -e "rmarkdown::render('_faq.Rmd', output_file='faq.html')"
-# git add faq.md images/faq/*.png
---
+++ _render_faq.sh
@@ -0,0 +1,2 @@
+Rscript -e "rmarkdown::render('_faq.Rmd', output_file='faq.md')"
+#Rscript -e "rmarkdown::render('_faq.Rmd', output_format=rmarkdown::html_document, output_file='faq.html')"
