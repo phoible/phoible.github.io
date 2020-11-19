d7982832ebb656af731662506643b0940c4ef017
Daniel McCloy <dan@mccloy.info>
2020-11-18 21:09:30 -0600

    render to HTML

--- _render_faq.sh
+++ _render_faq.sh
@@ -1,2 +1,3 @@
-Rscript -e "rmarkdown::render('_faq.Rmd', output_file='faq.md')"
+Rscript -e "rmarkdown::render('_faq.Rmd', output_file='faq.html')"
+#Rscript -e "rmarkdown::render('_faq.Rmd', output_file='faq.md')"
 #Rscript -e "rmarkdown::render('_faq.Rmd', output_format=rmarkdown::html_document, output_file='faq.html')"
