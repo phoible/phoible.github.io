e0301743725f172f125a502a0aabfc71fffcc550
Tom Liu <ltxom@uw.edu>
2021-04-08 17:51:59 -0700

    Fix styles

--- convertMdToHTML.py
+++ convertMdToHTML.py
@@ -60,10 +60,53 @@ def extract_content(file_path, output_path):
             table['cellpadding'] = '0'
             table['cellspacing'] = '0'
             table['border'] = '0'
-            table['class'] = 'table table-bordered order-column compact stripe dataTable no-footer'
+            table['class'] = 'table table-bordered order-column compact stripe dataTable no-footer table-nonfluid'
             table['role'] = 'grid'
         for th in ths:
             th['role'] = 'row'
+
+        ps = soup.find_all('p')
+        # fix blockquotes
+        for p in ps:
+            if '&gt;' in str(p):
+                p_temp = str(p).split('&gt;')
+                p.clear()
+                p.string = p_temp[0].replace('<p>','')
+                blockquote = soup.new_tag('blockquote')
+                blockquote.append(bs4.BeautifulSoup(p_temp[1][0: len(p_temp) - 6], 'html.parser'))
+                p.append(blockquote)
+        # fix <em> Spacing
+        ems = soup.find_all('em')
+        for em in ems:
+            em.string = em.get_text().strip()
+        # fix references
+        references_div = soup.find('div', {'class':'references'})
+        if references_div is not None:
+            references_ps = references_div.find_all('p')
+            for p in references_ps:
+                # fix url
+                if 'Online: urlhttp' in str(p):
+                    p_temp = p.get_text().split('Online: urlhttp')
+                    p.string = p_temp[0]
+                    a = soup.new_tag('a')
+                    a.string = 'http' + p_temp[1]
+                    a['href'] = a.string
+                    p.append(a)
+                # fix spacing
+                if ' ,' in str(p):
+                    p.string = re.sub(r' +,', ',', p.get_text())
+                if ' .' in str(p):
+                    p.string = re.sub(r' +.', '.', p.get_text())
+                if p.get_text().endswith(':'):
+                    p.string = p.get_text()[0 : len(p.get_text()) - 1] + '.'
+        # fix titles size
+        h2s = soup.find_all('h2')
+        for h2 in h2s:
+            h2.name = 'h3'
+        h1s = soup.find_all('h1')
+        for h1 in h1s:
+            h1.name = 'h2'
+
         div_content = soup.find('div', {'class': 'container-fluid main-container'})
         with open(output_path, 'w') as file:
             file.write(str(div_content))
@@ -85,12 +128,26 @@ def change_font(file_path, output_path):
 
 
 def change_table_font(file_path, output_path):
+    """
+    change the columns with name "phonemes" to charissil font
+    """
     with open(file_path) as f:
         soup = bs4.BeautifulSoup(f.read(), 'html.parser')
-        tds = soup.find_all('td')
-        for td in tds:
-            if is_valid_ipa(str(td.string).strip()):
-                td['class'] = 'charissil'
+        ths = soup.find_all('th')
+        for th in ths:
+            if th.get_text().lower() == 'phonemes':
+                ths_temp = th.parent.find_all('th')
+                index = -1
+                for th_temp in ths_temp:
+                    index += 1
+                    if th_temp.get_text().lower() == 'phonemes':
+                        break
+                table = th.parent.parent.parent
+                tbody = table.find('tbody')
+                trs = tbody.find_all('tr')
+                for tr in trs:
+                    td = tr.find_all('td')[index]
+                    td['class'] = 'charissil'
         div_content = soup.find('div', {'class': 'container-fluid main-container'})
         with open(output_path, 'w') as file:
             file.write(str(div_content))
@@ -101,9 +158,9 @@ def main():
     print('File: _faq.Rmd')
     os.system('Rscript --vanilla knitRmdToHTML.R _faq.Rmd ' + RSTUDIO_PANDOC)
     change_table_font('_faq.html', '_faq.html')
-    extract_content('_faq.html', '_faq.html')
-    print('Generating FAQ indexes...')
-    generate_indexes('_faq.html', 'faq_with_indexes.html')
+    extract_content('_faq.html', 'faq_with_indexes.html')
+    # print('Generating FAQ indexes...')
+    # generate_indexes('__faq.html', 'faq_with_indexes.html')
     print('File: conventions.Rmd')
     os.system('Rscript --vanilla knitRmdToHTML.R conventions.md ' + RSTUDIO_PANDOC)
     change_font('conventions.html','conventions.html')
